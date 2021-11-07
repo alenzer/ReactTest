ab369d2fdd9cb1290b07ae66af7650d49890ba0f
alenzer <alenzer0902@gmail.com>
2021-11-07 20:01:58 +0100

    Testing

---
+++ src/Bathtub.js
@@ -0,0 +1,55 @@
+import './App.css';
+import { useState, useRef } from "react";
+
+function Bathtub() {
+  const [count, setCount] = useState(0);
+
+  const timer = useRef();
+
+  function FillBath(type){
+    if(type === 'up'){
+      clearInterval(timer.current);
+      timer.current = setInterval(() => {
+        setCount(count => count+1);
+      }, 1000);
+    }
+    else if(type == 'down'){
+      clearInterval(timer.current);
+      timer.current = setInterval(() => {
+        setCount(count => count - 1);
+      }, 1000);
+    }
+  }
+
+  if(count > 4 || count < 1){
+    clearInterval(timer.current);
+  }
+  
+  let indents = [];
+  const myStyle = {'width':'100px', 'height':'50px'};
+
+  for (let i = 0; i < 5; i++) {
+    if(i < 5-count)
+      indents.push(<div key={"level"+i} style={{...myStyle, 'backgroundColor':'white'}}><span></span></div>);
+    else
+      indents.push((<div key={"level"+i} style={{...myStyle, 'backgroundColor':'blue'}}><span></span></div>));
+  }
+  return (
+    <>
+      <div style={{'display':'flex', 'justifyItems':'center'}} >
+        <div key="bathtub" style={{'backgroundColor':'yellow', 'border':'3'}}>
+          {indents}
+        </div>
+        <div>
+          {count}
+        </div>
+        <div>
+          <input value="Up" type="button" onClick={()=>FillBath('up')}></input>
+          <input value="Down" type="button" onClick={()=>FillBath('down')}></input>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Bathtub;
